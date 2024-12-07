528e3bf45f3dbddd43987bf0f0ae5496e9dc301d
MinhQuan21112002 <cobedt123@gmail.com>
2024-12-07 22:55:40 +0700

    Your commit message

--- src/global.js
+++ src/global.js
@@ -2,4 +2,5 @@
 // export const hostName="https://pandajob.cfapps.ap21.hana.ondemand.com"
 export const hostName="https://alert-emotion-production.up.railway.app"
 //export const hostName="http://localhost:8080"
-export const webHost="http://localhost:3000"
+//export const webHost="http://localhost:3000"
+export const webHost="https://kltnfrontendqltd-production.up.railway.app"
