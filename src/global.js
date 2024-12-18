1f6676d51c67c0e709da7e40cf96bd412fc675d3
MinhQuan21112002 <cobedt123@gmail.com>
2024-12-19 05:17:17 +0700

    Your commit message

--- src/global.js
+++ src/global.js
@@ -1,5 +1,5 @@
 
 export const hostName="https://alert-emotion-production.up.railway.app"
 //export const hostName="http://localhost:8080"
-//export const webHost="http://localhost:3000"
-export const webHost="https://kltnfrontendqltd-production.up.railway.app"
+export const webHost="http://localhost:3000"
+//export const webHost="https://kltnfrontendqltd-production.up.railway.app"
